e4d56d80ef5e6563323402f5ebcbfa8b3f7223c2
Thomas Lehmann <lehmann@ualberta.ca>
2018-04-20 11:38:30 -0600

    Add various services (move_to_rpy, move_to_quat, start_teleop)
    * 'move_to_rpy' & 'move_to_quat' have been tested in simulation and are
      working
    * 'start_teleop' and teleoperation implementation itself have not been
      tested

--- src/moveit_interface.cpp
+++ src/moveit_interface.cpp
@@ -10,13 +10,20 @@
 //      The service is to be called after the service 'move_to'.
 
 #include <moveit/move_group_interface/move_group_interface.h>
+#include <ros/callback_queue.h>
 #include <ros/ros.h>
 
+#include <tf/tf.h>
+
 #include "motoman_sia5f_teleop/MoveHome.h"
+#include "motoman_sia5f_teleop/MoveToQuat.h"
+#include "motoman_sia5f_teleop/MoveToRPY.h"
+#include "motoman_sia5f_teleop/StartTeleop.h"
 #include <vector>
 
 static const std::string PLANNING_GROUP = "manipulator";
 geometry_msgs::Pose g_pose_ref;
+bool g_do_teleop = false;
 
 void callbackPose(const geometry_msgs::Pose::ConstPtr &msg) {
   // TODO Make sure that the duration for planning is limited to the sampling
@@ -45,7 +52,7 @@ bool callbackMoveHome(motoman_sia5f_teleop::MoveHome::Request &req,
   if (success) {
     if (move_group.execute(plan) ==
         moveit::planning_interface::MoveItErrorCode::SUCCESS) {
-          res.reply = 0;
+      res.reply = 0;
       return true;
     } else {
       res.reply = -1;
@@ -54,49 +61,172 @@ bool callbackMoveHome(motoman_sia5f_teleop::MoveHome::Request &req,
   }
 }
 
+bool callbackMoveToRPY(motoman_sia5f_teleop::MoveToRPY::Request &req,
+                       motoman_sia5f_teleop::MoveToRPY::Response &res) {
+
+  moveit::planning_interface::MoveGroupInterface move_group(PLANNING_GROUP);
+  ROS_INFO("Max velocity scaling factor: %1.1f", req.max_vel_fact);
+  ROS_INFO("Max acceleration scaling factor: %1.1f", req.max_acc_fact);
+  tf::Quaternion tf_q;
+  tf_q.setEuler(req.pose_rpy[5], req.pose_rpy[4], req.pose_rpy[3]);
+  geometry_msgs::Pose pose;
+  pose.position.x = req.pose_rpy[0];
+  pose.position.y = req.pose_rpy[1];
+  pose.position.z = req.pose_rpy[2];
+  pose.orientation.x = tf_q.getX();
+  pose.orientation.y = tf_q.getY();
+  pose.orientation.z = tf_q.getZ();
+  pose.orientation.w = tf_q.getW();
+
+  move_group.setPoseTarget(pose, "link_t");
+
+  // NOTE There seems to be some issue with the setRPYTarget()-function of
+  //      MoveGroupInterface. Supplying the same pose multiple times results in
+  //      the robot changing its pose while it should not move at all.
+  /*
+  move_group.setPositionTarget(req.pose_rpy[0], req.pose_rpy[1],
+                               req.pose_rpy[2], "");
+  move_group.setRPYTarget(req.pose_rpy[3], req.pose_rpy[4], req.pose_rpy[5],
+                          "");*/
+  ROS_INFO(
+      "Planning and executing pose goal [%1.2f %1.2f %1.2f %1.2f %1.2f %1.2f]",
+      req.pose_rpy[0], req.pose_rpy[1], req.pose_rpy[2], req.pose_rpy[3],
+      req.pose_rpy[4], req.pose_rpy[5]);
+  moveit::planning_interface::MoveGroupInterface::Plan plan;
+
+  bool success = (move_group.plan(plan) ==
+                  moveit::planning_interface::MoveItErrorCode::SUCCESS);
+
+  if (success) {
+    if (move_group.execute(plan) ==
+        moveit::planning_interface::MoveItErrorCode::SUCCESS) {
+      res.reply = 0;
+      return true;
+    } else {
+      res.reply = -1;
+      return false;
+    }
+  }
+}
+
+bool callbackMoveToQuat(motoman_sia5f_teleop::MoveToQuat::Request &req,
+                        motoman_sia5f_teleop::MoveToQuat::Response &res) {
+
+  moveit::planning_interface::MoveGroupInterface move_group(PLANNING_GROUP);
+  ROS_INFO("Max velocity scaling factor: %1.1f", req.max_vel_fact);
+  ROS_INFO("Max acceleration scaling factor: %1.1f", req.max_acc_fact);
+
+  geometry_msgs::Pose pose;
+  pose.position.x = req.pose_quat[0];
+  pose.position.y = req.pose_quat[1];
+  pose.position.z = req.pose_quat[2];
+  pose.orientation.x = req.pose_quat[3];
+  pose.orientation.y = req.pose_quat[4];
+  pose.orientation.z = req.pose_quat[5];
+  pose.orientation.w = req.pose_quat[6];
+  move_group.setPoseTarget(pose, "link_t");
+  ROS_INFO("Planning and executing pose goal [%1.2f %1.2f %1.2f %1.2f %1.2f "
+           "%1.2f  %1.2f]",
+           req.pose_quat[0], req.pose_quat[1], req.pose_quat[2],
+           req.pose_quat[3], req.pose_quat[4], req.pose_quat[5],
+           req.pose_quat[6]);
+  moveit::planning_interface::MoveGroupInterface::Plan plan;
+
+  bool success = (move_group.plan(plan) ==
+                  moveit::planning_interface::MoveItErrorCode::SUCCESS);
+
+  if (success) {
+    if (move_group.execute(plan) ==
+        moveit::planning_interface::MoveItErrorCode::SUCCESS) {
+      res.reply = 0;
+      return true;
+    } else {
+      res.reply = -1;
+      return false;
+    }
+  }
+}
+
+bool callbackStartTeleop(motoman_sia5f_teleop::StartTeleop::Request &req,
+                         motoman_sia5f_teleop::StartTeleop::Response &res) {
+
+  g_do_teleop = true;
+  res.reply = 0;
+  return 0;
+}
+
 int main(int argc, char **argv) {
 
-  // --- Initializations ---
+  // --- Initializations
   ros::init(argc, argv, "moveit_interface");
-  ros::NodeHandle node_handle;
+
+  // --- Setup node handles for
+  //     - teleoperation callbacks
+  //     - move_home and move_to service callbacks
+  //     - teleop_start and teleop_stop callbacks
+  ros::NodeHandle nh_teleop;
+  ros::NodeHandle nh_move;
+  ros::NodeHandle nh_startstop;
+
+  // --- Setup custom callback queues for
+  //     - move_home service callbacks
+  //     - teleop_start and teleop_stop callbacks
+  ros::CallbackQueue queue_move;
+  nh_move.setCallbackQueue(&queue_move);
+  ros::CallbackQueue queue_startstop;
+  nh_startstop.setCallbackQueue(&queue_startstop);
 
   // --- Start an AsyncSpinner with two threads for
   //     - trajectory planning and execution and
   //     - calls to service 'move_home'.
-  ros::AsyncSpinner spinner(2);
-  spinner.start();
+  ros::AsyncSpinner spin_move_home(1, &queue_move);
+  spin_move_home.start();
+  ros::AsyncSpinner spin_startstop(1, &queue_startstop);
+  spin_startstop.start();
+  ros::AsyncSpinner spin_plan(1);
+  spin_plan.start();
 
   moveit::planning_interface::MoveGroupInterface move_group(PLANNING_GROUP);
 
-  // --- Serup topic subscritsions
-  ros::Subscriber sub_pose = node_handle.subscribe("pose", 1, callbackPose);
+  // --- Setup topic subscritions
+  ros::Subscriber sub_pose = nh_teleop.subscribe("pose", 1, callbackPose);
 
   // --- Advertise services
   //     - move_home
-  ros::ServiceServer srv_move_home = node_handle.advertiseService(
-      "/moveit_interface/move_home", callbackMoveHome);
+  //     - move_to
+  //     - start_teleop
+  ros::ServiceServer srv_move_home =
+      nh_move.advertiseService("/moveit_interface/move_home", callbackMoveHome);
+  ros::ServiceServer srv_move_to_rpy = nh_move.advertiseService(
+      "/moveit_interface/move_to_rpy", callbackMoveToRPY);
+  ros::ServiceServer srv_move_to_quat = nh_move.advertiseService(
+      "/moveit_interface/move_to_quat", callbackMoveToQuat);
+  ros::ServiceServer srv_start = nh_startstop.advertiseService(
+      "/moveit_interface/start_teleop", callbackStartTeleop);
 
   // --- Obtain parameters
   int rate_hz = 10;
-  node_handle.getParam("moveit_interface/rate", rate_hz);
+  nh_teleop.getParam("moveit_interface/rate", rate_hz);
   ros::Rate loop_rate(rate_hz);
   move_group.setPlanningTime(1.0 / (double)rate_hz -
                              1.0 / (2 * (double)rate_hz));
 
   while (ros::ok()) {
 
-    ros::spinOnce();
-    /*
-        move_group.setPoseTarget(g_pose_ref);
-        moveit::planning_interface::MoveGroupInterface::Plan plan;
+    if (g_do_teleop) {
+      ros::spinOnce();
 
-        bool success = (move_group.plan(plan) ==
-                        moveit::planning_interface::MoveItErrorCode::SUCCESS);
+      move_group.setPoseTarget(g_pose_ref);
+      moveit::planning_interface::MoveGroupInterface::Plan plan;
 
-        if (success) {
-          move_group.execute(plan);
-        }
-    */
+      bool success = (move_group.plan(plan) ==
+                      moveit::planning_interface::MoveItErrorCode::SUCCESS);
+
+      if (success) {
+        move_group.execute(plan);
+      }
+    }
+
     // Loop until user aborts (Ctrl+C)
     loop_rate.sleep();
   }
